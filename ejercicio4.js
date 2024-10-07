a9b141d8f97ef964d62c655cad7c2fc4f9bb5530
Sara Vergara <sarasilvia.vergara@gmail.com>
2024-10-07 15:32:35 -0600

    feat: se completó el ejercicio 4

--- ejercicio4.js
+++ ejercicio4.js
@@ -4,3 +4,11 @@ Create a program that loops over the 2 arrays; if there are any common courses p
 
 let student1Courses = ['Math', 'English', 'Programming'];
 let student2Courses = ['Geography', 'Spanish', 'Programming'];
+
+//Uso de filter e includes
+
+const commonCourses = student1Courses.filter((assigments) => student2Courses.includes(assigments));
+console.log(commonCourses);
+
+    document.getElementById("common course").textContent = "Common courses: " + commonCourses;
+    
--- ejercicio5.js
+++ ejercicio5.js
@@ -1,20 +1,36 @@
 
 let people = ["Maria", "Dani", "Luis", "Juan", "Camila"];
+document.getElementById("array").textContent = people;
 
 //Eliminar Dani del array
 people.splice(1, 1);
-
+document.getElementById("dani_eliminado").textContent = people;
 
 //Eliminar a Juan del array
 people.splice(2, 1);
-
+document.getElementById("juan_eliminado").textContent = people;
 
 //Mover a "Luis" al frente de la matriz
-const luis = people.splice(1, 1);
-people.unshift(luis[0]);
+people.splice(1, 1);
+people.unshift("Luis");
+document.getElementById("mover_luis").textContent = people;
 
-
-//Agregar su nombre al final de la matriz
+//Agregar mi nombre al final de la matriz
 people.push("Sara");
+document.getElementById("mi_nombre").textContent = people;
+
+// Bucle
+/*for (let i = 0; i < person.length; i++) {
+    console.log(person);
+    if (person === "Maria") {
+        break;
+    }
+}*/
 
 
+// indexOf donde se encuentra "Maria"
+const indexOfMaria = people.indexOf("Maria");
+document.getElementById("indexOfMaria").textContent = indexOfMaria;
+
+// 4 personas en la formación
+document.getElementById("finalArray").textContent = people;
